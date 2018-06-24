6d253ba5c3223096567532424350369cb01c50eb
JosephAMork <40401700+JosephAMork@users.noreply.github.com>
2018-06-24 15:06:20 +0200

    Update newsBBC.py

--- newsBBC.py
+++ newsBBC.py
@@ -1,4 +1,7 @@
 # to install lib  use  pip install feedparser  OR   easy_install feedparser
+# FB Group: Kali linux and blackarch tools
+# edit by Joseph A Morcos
+# python 3.7 with feedparser lib
 from datetime import datetime
 import feedparser
 BBC_FEED_URL='http://feeds.bbci.co.uk/news/%s/rss.xml'
@@ -9,17 +12,17 @@ def read_news(feed_url):
 	except Exception as e:
 		print("Got error : %s" %str(e))
 		
-		for entry in data.enteries:
-			print((entry.title))
-			ptiny((entry.link))
-			print((entry.description))
-			print("\n")
-			print("\n")
+	for entry in data.enteries:
+		print((entry.title))
+		ptiny((entry.link))
+		print((entry.description))
+		print("\n")
+		print("\n")
 			
-		if __name__=="__main__":
-			print("=== Reading news feed from BBC (%s)===== " %datetime.today())
-			print("Enter the type of news feed :")
-			print("Available options are : world, uk, health, sci-tech, business, technology")
-			type=raw_input("news feed type :")
-			read_news(BBC_FEED_URL%type)
-			print("+++ end of BBC news feed+++++")
+	if __name__=="__main__":
+		print("=== Reading news feed from BBC (%s)===== " %datetime.today())
+		print("Enter the type of news feed :")
+		print("Available options are : world, uk, health, sci-tech, business, technology")
+		type=raw_input("news feed type :")
+		read_news(BBC_FEED_URL%type)
+		print("+++ end of BBC news feed+++++")
