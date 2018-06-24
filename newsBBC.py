3ab1d63da9eb83119647c6a1a20dde1e903ef70a
JosephAMork <josephmorcos6@gmail.com>
2018-06-24 14:54:27 +0200

    first V

---
+++ newsBBC.py
@@ -0,0 +1,25 @@
+# to install lib  use  pip install feedparser  OR   easy_install feedparser
+from datetime import datetime
+import feedparser
+BBC_FEED_URL='http://feeds.bbci.co.uk/news/%s/rss.xml'
+
+def read_news(feed_url):
+	try:
+		data=feedparser.parse(feed_url)
+	except Exception as e:
+		print("Got error : %s" %str(e))
+		
+		for entry in data.enteries:
+			print((entry.title))
+			ptiny((entry.link))
+			print((entry.description))
+			print("\n")
+			print("\n")
+			
+		if __name__=="__main__":
+			print("=== Reading news feed from BBC (%s)===== " %datetime.today())
+			print("Enter the type of news feed :")
+			print("Available options are : world, uk, health, sci-tech, business, technology")
+			type=raw_input("news feed type :")
+			read_news(BBC_FEED_URL%type)
+			print("+++ end of BBC news feed+++++")
